69c5991c4b67ebac8518aee24c6ce292de054683
Chitti Babu Theegala <ctheegal@codeaurora.org>
2014-03-20 14:11:21 +0530

    lowmemorykiller: Overwrite LMK parameters for low-tier devices
    LMK parameters calculated by default algorithm were not optimal for
    low-tier devices. Values mentioned here were well tested and
    evaluated w.r.t memory and performance for such setups.
    
    Change-Id: I40ed92d204aae6e758028ea1e9bd4ec7226dbe2b
    
    lowmemorykiller: fine-tune LMK parameters for low-tier devices
    
    LMK parameters for low-tier devices are fine-tuned for better
    concurrency. Values mentioned here were well tested and
    evaluated w.r.t memory and performance for such setups.
    
    Change-Id: I0d547f1ec84863bbaedb68d9aba1ff7aef057e42
    
    [mikeioannina]: Adjust for 5.0
    
    Change-Id: I258b1058d0feadbd153c0068d149db8d8b71c93f
    
    Conflicts:
    	services/core/java/com/android/server/am/ProcessList.java

--- services/core/java/com/android/server/am/ProcessList.java
+++ services/core/java/com/android/server/am/ProcessList.java
@@ -181,6 +181,11 @@ final class ProcessList {
     };
     // The actual OOM killer memory levels we are using.
     private final int[] mOomMinFree = new int[mOomAdj.length];
+    // Optimal OOM killer memory levels for Low-Tier devices.
+    private final int[] mOomMinFreeLowRam = new int[] {
+            12288, 20478, 32766,
+            40962, 49152, 57342
+    };
 
     private final long mTotalMemMb;
 
@@ -238,14 +243,19 @@ final class ProcessList {
         final boolean is64bit = Build.SUPPORTED_64_BIT_ABIS.length > 0;
 
         for (int i=0; i<mOomAdj.length; i++) {
-            int low = mOomMinFreeLow[i];
-            int high = mOomMinFreeHigh[i];
-            if (is64bit) {
-                // Increase the high min-free levels for cached processes for 64-bit
-                if (i == 4) high = (high*3)/2;
-                else if (i == 5) high = (high*7)/4;
+        if (ActivityManager.isLowRamDeviceStatic()) {
+        // Overwrite calculated LMK parameters with the low-tier tested/validated values
+        mOomMinFree[i] = mOomMinFreeLowRam[i];
+            } else {
+                int low = mOomMinFreeLow[i];
+                int high = mOomMinFreeHigh[i];
+                if (is64bit) {
+                    // Increase the high min-free levels for cached processes for 64-bit
+                    if (i == 4) high = (high*3)/2;
+                    else if (i == 5) high = (high*7)/4;
+                }
+                mOomMinFree[i] = (int)(low + ((high-low)*scale));
             }
-            mOomMinFree[i] = (int)(low + ((high-low)*scale));
         }
 
         if (minfree_abs >= 0) {
