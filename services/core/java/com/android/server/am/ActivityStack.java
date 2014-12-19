3627c47010bf4a84faed9f19a4e482b40ad92303
louis_chang <louis_chang@htc.com>
2014-12-19 17:30:45 +0800

    [ActivityManager] Reset ActivityRecord's waitingVisible
    Symptom:
    The ActivityRecord's waitingVisible is not set to false for
    all cases when the ActivityRecord is removed from
    mStackSupervisor.mWaitingVisibleActivities.
    
    Solution:
    Set the waitingVisible to false when it is removed from
    mStackSupervisor.mWaitingVisibleActivities
    
    Change-Id: If0f0683971d70a6866b0167e91dd95feebaac178

--- services/core/java/com/android/server/am/ActivityStack.java
+++ services/core/java/com/android/server/am/ActivityStack.java
@@ -1574,6 +1574,7 @@ final class ActivityStack {
         mStackSupervisor.mGoingToSleepActivities.remove(next);
         next.sleeping = false;
         mStackSupervisor.mWaitingVisibleActivities.remove(next);
+        next.waitingVisible = false;
 
         if (DEBUG_SWITCH) Slog.v(TAG, "Resuming " + next);
 
@@ -2831,6 +2832,7 @@ final class ActivityStack {
         mStackSupervisor.mStoppingActivities.remove(r);
         mStackSupervisor.mGoingToSleepActivities.remove(r);
         mStackSupervisor.mWaitingVisibleActivities.remove(r);
+        r.waitingVisible = false;
         if (mResumedActivity == r) {
             mResumedActivity = null;
         }
@@ -3032,6 +3034,7 @@ final class ActivityStack {
         // down to the max limit while they are still waiting to finish.
         mStackSupervisor.mFinishingActivities.remove(r);
         mStackSupervisor.mWaitingVisibleActivities.remove(r);
+        r.waitingVisible = false;
 
         // Remove any pending results.
         if (r.finishing && r.pendingResults != null) {
