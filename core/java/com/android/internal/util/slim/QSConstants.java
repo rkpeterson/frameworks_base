334815300c4d92b8a7aeca83bfe822d01525c913
Danesh M <daneshm90@gmail.com>
2015-01-23 18:59:21 -0800

    SystemUI : Add screen timeout tile
    Change-Id: I0854e940f9604dee2fff5093e688ef3a17b258a9

--- core/java/com/android/internal/util/slim/QSConstants.java
+++ core/java/com/android/internal/util/slim/QSConstants.java
@@ -42,6 +42,7 @@ public class QSConstants {
     public static final String TILE_LOCKSCREEN = "lockscreen";
     public static final String TILE_LTE = "lte";
     public static final String TILE_VISUALIZER = "visualizer";
+    public static final String TILE_SCREEN_TIMEOUT = "screen_timeout";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -74,5 +75,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_LOCKSCREEN);
         TILES_AVAILABLE.add(TILE_LTE);
         TILES_AVAILABLE.add(TILE_VISUALIZER);
+        TILES_AVAILABLE.add(TILE_SCREEN_TIMEOUT);
     }
 }
