36b9bec01da23aa071f4d330be13b5476b139e1e
Roman Birg <roman@cyngn.com>
2015-02-16 11:09:40 -0800

    SystemUI: fix flashlight tile inconsistency
    Toggling the torch via an external method (such as drawing V on
    screen) would put the tile in a strange state and hide the tile.
    
    Update the tile availability properly.
    
    Change-Id: I597853b3ff3822e24bf512bbaefd9e192394c390
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/FlashlightTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/FlashlightTile.java
@@ -72,12 +72,11 @@ public class FlashlightTile extends QSTile<QSTile.BooleanState> implements
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
-        if (state.value) {
-            mWasLastOn = SystemClock.uptimeMillis();
-        }
-
         if (arg instanceof Boolean) {
             state.value = (Boolean) arg;
+        }
+        if (state.value) {
+            mWasLastOn = SystemClock.uptimeMillis();
         }
 
         if (!state.value && mWasLastOn != 0) {
@@ -122,7 +121,7 @@ public class FlashlightTile extends QSTile<QSTile.BooleanState> implements
 
     @Override
     public void onTorchAvailabilityChanged(boolean available) {
-        refreshState();
+        refreshState(mTorchManager.isTorchOn());
     }
 
     private Runnable mRecentlyOnTimeout = new Runnable() {
