802c71e171e16aa0349122b1397adba9a2fef8c4
Roman Birg <roman@cyngn.com>
2015-03-10 17:21:27 -0700

    SystemUI: allow lock screen visualizer to be disabled
    Change-Id: I36a81071ad0d8f898b17a5e72ebea48672e62b9c
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5776,6 +5776,13 @@ public final class Settings {
         public static final String QS_LOCATION_ADVANCED = "qs_location_advanced";
 
         /**
+         * Whether to show the lockscreen visualizer.
+         * 0 will disable it, anything else will enable it. Default is on.
+         * @hide
+         */
+        public static final String LOCKSCREEN_VISUALIZER_ENABLED = "lockscreen_visualizer";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
