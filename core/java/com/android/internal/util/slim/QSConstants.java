46f9600b859952ac8470f803b7ef91f381419ff2
RobbieL811 <RobbieLancaster811>
2015-03-27 03:53:55 -0600

    Remove Notifications tile
    Change-Id: Ie642ed556b682e8b0bd3c10468adbfec47b73816

--- core/java/com/android/internal/util/slim/QSConstants.java
+++ core/java/com/android/internal/util/slim/QSConstants.java
@@ -31,7 +31,6 @@ public class QSConstants {
     public static final String TILE_LOCATION = "location";
     public static final String TILE_CAST = "cast";
     public static final String TILE_HOTSPOT = "hotspot";
-    public static final String TILE_NOTIFICATIONS = "notifications";
     public static final String TILE_DATA = "data";
     public static final String TILE_ROAMING = "roaming";
     public static final String TILE_DDS = "dds";
@@ -64,7 +63,6 @@ public class QSConstants {
         TILES_AVAILABLE.addAll(TILES_DEFAULT);
         TILES_AVAILABLE.add(TILE_INVERSION);
         TILES_AVAILABLE.add(TILE_HOTSPOT);
-        TILES_AVAILABLE.add(TILE_NOTIFICATIONS);
         TILES_AVAILABLE.add(TILE_DATA);
         TILES_AVAILABLE.add(TILE_ROAMING);
         TILES_AVAILABLE.add(TILE_DDS);
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -52,7 +52,6 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.LockscreenToggleTile;
 import com.android.systemui.qs.tiles.LteTile;
-import com.android.systemui.qs.tiles.NotificationsTile;
 import com.android.systemui.qs.tiles.RoamingTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.VisualizerTile;
@@ -301,8 +300,6 @@ public class QSTileHost implements QSTile.Host {
                 return new CastTile(this);
             case QSConstants.TILE_HOTSPOT:
                 return new HotspotTile(this);
-            case QSConstants.TILE_NOTIFICATIONS:
-                return new NotificationsTile(this);
             case QSConstants.TILE_DATA:
                 return new DataTile(this);
             case QSConstants.TILE_ROAMING:
